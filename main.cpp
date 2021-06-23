7e06bc5b57bfeab06f5ac215274d792cbad8163c
OldScripter <Oldscripter@gmail.com>
2021-06-23 23:19:33 +0300

    First corrections are implemented

--- main.cpp
+++ main.cpp
@@ -1,72 +1,41 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
-
-/*
- * Get the sum of all <int> vector members.
- */
-int getSum(std::vector<int>& v)
-{
-    int sum = 0;
-    for (int i = 0; i < v.size(); i++)
-    {
-        sum += v[i];
-    }
-    return sum;
-}
-
-/*
- * Get the index of maximal <int> vector member.
- */
-int getIndexOfMax(std::vector<int>& v)
-{
-    int max_i = 0;
-    int max = 0;
-    for (int i = 0; i < v.size(); i++)
-    {
-        if (v[i] > max)
-        {
-            max_i = i;
-            max = v[i];
-        }
-
-    }
-    return max_i;
-}
 
 int main() {
-    char *dir = "..\\table_data.txt";
-    std::vector<std::string> names;
-    std::vector<std::string> surnames;
-    std::vector<int> payments;
-    std::vector<std::string> dates;
+    const std::string FILE_PATH = "..\\table_data.txt";
 
-    std::ifstream stream(dir, std::ios::binary);
-    if (stream.is_open()) {
-        while (!stream.eof()) {
-            std::string name;
-            std::string surname;
-            int payment;
-            std::string date;
+    std::string name;
+    std::string surname;
+    int payment;
+    std::string date;
 
-            stream >> name >> surname >> payment >> date;
+    int maxPayment = 0;
+    int sumPayment = 0;
+    std::string nameMaxPayment = " ";
+    std::string surnameMaxPayment = " ";
 
-            names.push_back(name);
-            surnames.push_back(surname);
-            payments.push_back(payment);
-            dates.push_back(date);
-        }
-    } else
+    std::ifstream inputStream(FILE_PATH, std::ios::binary);
+    if (inputStream.is_open())
     {
-        std::cout << "Bad file or path.\n";
+        while (!inputStream.eof())
+        {
+            inputStream >> name >> surname >> payment >> date;
+            if (payment > maxPayment)
+            {
+                maxPayment = payment;
+                nameMaxPayment = name;
+                surnameMaxPayment = surname;
+            }
+            sumPayment += payment;
+        }
+        inputStream.close();
+        std::cout << "Max payment is: " << maxPayment << "\n";
+        std::cout << "Max payment owner is: " << nameMaxPayment << " " << surnameMaxPayment << "\n";
+        std::cout << "Sum of all payments is: " << sumPayment << "\n";
     }
-    stream.close();
-    printf("Sum of all payments: %u $\n", getSum(payments));
-
-    int maxValueIndex = getIndexOfMax(payments);
-    std::cout << "Maximal payment was paid to: " << names[maxValueIndex] << " ";
-    std::cout << surnames[maxValueIndex] << " - ";
-    std::cout << payments[maxValueIndex] << " $\n";
-
+    else
+    {
+        std::cerr << "File read error. Check path.\n";
+    }
     return 0;
 }
